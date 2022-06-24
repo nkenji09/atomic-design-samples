af3e0573a8670fe49e8ca9a7b1fa506ac6dbd3f8
kenji nakanishi <nakanishikenji0929@gmail.com>
2022-06-24 13:50:26 +0900

    feat: Normal Button

---
+++ app/src/components/atoms/buttons/normal-button.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { defineEmits } from "vue";
+
+const emits = defineEmits<{
+  (e: "click"): void;
+}>();
+
+const onClick = () => {
+  emits("click");
+};
+</script>
+
+<template>
+  <button @click="onClick">
+    <slot>CLICK ME</slot>
+  </button>
+</template>
